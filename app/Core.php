c6da9ef061feb614b9bbbc6bf8592a7aed3a3089
elmhmmd <youcodemohammed@gmail.com>
2025-02-04 16:45:58 +0100

    initial commit

---
+++ app/Core.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace 
+
+class Core {
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    public function __construct() {
+        $this->getUrl();
+    }
+
+    public function getUrl() {
+        echo $_GET['url'];
+
+    }
+
+}
---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use myapp/
+
+
+$init = new Core();
+
+
+/*require __DIR__ . '/../vendor/autoload.php';
+
+$requestUri = $_SERVER['REQUEST_URI'];
+
+$requestPath = strtok($requestUri, '?');
+
+switch($requestPath) {
+   
+    case '/':
+
+
+
+
+
+}*/
